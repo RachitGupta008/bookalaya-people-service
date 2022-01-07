7bf53601f9a8ac16d03a2740b0e5ebd0a18b90d4
Rachit <rachit.gupta97@yahoo.com>
2022-01-07 18:06:18 +0530

    Peopleservice starting commit

---
+++ src/main/java/com/bookalaya/peopleservice/PeopleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bookalaya.peopleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookalaya/peopleservice/PeopleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookalaya.peopleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
